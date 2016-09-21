ca556f3f77fea41786b05887866a9f597eea2c15
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-09-21 16:55:21 +0200

    initial kill procedures on candidates

--- leftokill.py
+++ leftokill.py
@@ -40,15 +40,37 @@ def init_syslog():
 def daemon_func(logger):
     while True:
         pt = psutil.process_iter()
+        candidate_list = list()
+        report_info = dict()
+
         for p in pt:
             if p.ppid() == 1:
                 homedir = pwd.getpwnam(p.username())[5]
                 if homedir.startswith(homeprefix):
-                    logger.info('Candidate:(%s) User:(%s) PID:(%d) Created:(%s) Status:(%s) CPU:(user=%s, sys=%s) Memory:(RSS=%s) CMD:(%s)' \
-                                % (p.name(), p.username(),  p.pid,
-                                   datetime.datetime.fromtimestamp(p.create_time()).strftime("%Y-%m-%d %H:%M:%S"),
-                                   p.status(), p.cpu_times()[0], p.cpu_times()[1],
-                                   bytes2human(p.memory_info()[0]), ' '.join(p.cmdline())))
+                    candidate_list.append(p)
+                    report_info.update({p.pid: {'name': p.name(), 'username': p.username(),
+                                                'created': datetime.datetime.fromtimestamp(p.create_time()).strftime("%Y-%m-%d %H:%M:%S"),
+                                                'status': p.status(), 'cpuuser': p.cpu_times()[0], 'cpusys': p.cpu_times()[1],
+                                                'rss': bytes2human(p.memory_info()[0]), 'cmdline': ' '.join(p.cmdline())}})
+                    curdict = report_info[p.pid]
+                    logger.info('PID:(%d) Candidate:(%s) User:(%s) Created:(%s) Status:(%s) CPU:(user=%s, sys=%s) Memory:(RSS=%s) CMD:(%s)' \
+                                % (p.pid, curdict['name'], curdict['username'], curdict['created'],
+                                   curdict['status'], curdict['cpuuser'], curdict['cpusys'],
+                                   curdict['rss'], curdict['cmdline']))
+
+        if candidate_list:
+            for p in candidate_list:
+                p.terminate()
+            gone, alive = psutil.wait_procs(candidate_list, timeout=10)
+            if gone:
+                for p in gone:
+                    logger.info('SIGTERM - PID:(%d) Candidate:(%s) User:(%s) Returncode:(%s)' \
+                                % (p.pid, report_info[p.pid]['name'], report_info[p.pid]['username'], p.returncode))
+            for p in alive:
+                p.kill()
+                logger.info('SIGKILL - PID:(%d) Candidate:(%s) User:(%s) Returncode:(%s)' \
+                            % (p.pid, report_info[p.pid]['name'], report_info[p.pid]['username'], p.returncode))
+
         time.sleep(15)
 
 def main():
