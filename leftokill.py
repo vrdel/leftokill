c126aaeda7f5e9fc349e48c39062cf2c6a5b70cc
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-09-22 21:40:21 +0200

    kill child processes too

--- leftokill.py
+++ leftokill.py
@@ -46,20 +46,23 @@ def daemon_func():
         pt = psutil.process_iter()
         candidate_list = list()
         report_entry = dict()
-
+        childs = dict()
         for p in pt:
             if p.ppid() == 1:
                 homedir = pwd.getpwnam(p.username())[5]
                 if homedir.startswith(homeprefix):
                     candidate_list.append(p)
+                    proc_childs = p.children(recursive=True)
+                    if len(proc_childs) > 0:
+                        childs[p.pid] = proc_childs
                     report_entry.update({p.pid: {'name': p.name(), 'username': p.username(),
                                                 'created': datetime.datetime.fromtimestamp(p.create_time()).strftime("%Y-%m-%d %H:%M:%S"),
                                                 'status': p.status(), 'cpuuser': p.cpu_times()[0], 'cpusys': p.cpu_times()[1],
                                                 'rss': bytes2human(p.memory_info()[0]), 'cmdline': ' '.join(p.cmdline())}})
-                    logger.info('PID:(%d) Candidate:(%s) User:(%s) Created:(%s) Status:(%s) CPU:(user=%s, sys=%s) Memory:(RSS=%s) CMD:(%s)' \
+                    logger.info('PID:(%d) Candidate:(%s) User:(%s) Created:(%s) Status:(%s) Childs:(%d) CPU:(user=%s, sys=%s) Memory:(RSS=%s) CMD:(%s)' \
                                 % (p.pid, report_entry[p.pid]['name'], report_entry[p.pid]['username'], report_entry[p.pid]['created'],
-                                   report_entry[p.pid]['status'], report_entry[p.pid]['cpuuser'], report_entry[p.pid]['cpusys'],
-                                   report_entry[p.pid]['rss'], report_entry[p.pid]['cmdline']))
+                                   report_entry[p.pid]['status'], len(proc_childs), report_entry[p.pid]['cpuuser'],
+                                   report_entry[p.pid]['cpusys'], report_entry[p.pid]['rss'], report_entry[p.pid]['cmdline']))
 
         if candidate_list:
             for p in candidate_list:
@@ -73,6 +76,20 @@ def daemon_func():
                 p.kill()
                 logger.info('SIGKILL - PID:(%d) Candidate:(%s) User:(%s)' \
                             % (p.pid, report_entry[p.pid]['name'], report_entry[p.pid]['username']))
+
+        if childs.get(p.pid):
+            for c in childs[p.pid]:
+                c.terminate()
+            gone, alive = psutil.wait_procs(childs[p.pid], timeout=10)
+            if gone:
+                for c in gone:
+                    logger.info('SIGTERM CHILD - PID:(%d) Candidate:(%s) User:(%s) Returncode:(%s)' \
+                                % (c.pid, report_entry[p.pid]['name'], report_entry[p.pid]['username'], c.returncode))
+            for c in alive:
+                c.kill()
+                logger.info('SIGKILL CHILD - PID:(%d) Candidate:(%s) User:(%s)' \
+                            % (c.pid, report_entry[p.pid]['name'], report_entry[p.pid]['username']))
+
 
         time.sleep(15)
 
@@ -114,9 +131,9 @@ def main():
     parse_config(conffile)
 
 
-    context_daemon = daemon.DaemonContext()
-    with context_daemon:
-        daemon_func()
-    # daemon_func()
+    #context_daemon = daemon.DaemonContext()
+    #with context_daemon:
+    #    daemon_func()
+    daemon_func()
 
 main()
