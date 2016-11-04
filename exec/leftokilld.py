46c842811af0908c15935872b04568b49926d00d
Daniel Vrcic <daniel.vrcic@gmail.com>
2016-11-04 12:28:44 +0100

    conf parse and daemonizing moved to exec

--- exec/leftokilld.py
+++ exec/leftokilld.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python
 
+from leftokill import config
 from leftokill import core
+from leftokill import log
+import argparse
+import daemon
 
-core.run()
+conffile = '/etc/leftokill/leftokill.conf'
+
+def main():
+    lobj = log.Logger()
+    logger = lobj.get()
+
+    confopts = config.parse_config(conffile, logger)
+
+    for l in confopts['logmode']:
+        if l.lower() == 'syslog':
+            lobj.init_syslog()
+        if l.lower() == 'file':
+            lobj.init_filelog()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', dest='nofork', action='store_true',
+                        help="do not fork into background",
+                        required=False)
+    args = parser.parse_args()
+
+    if args.nofork:
+        try:
+            core.run(confopts, logger)
+        except KeyboardInterrupt:
+            raise SystemExit(1)
+    else:
+        context_daemon = daemon.DaemonContext()
+        with context_daemon:
+            core.run(confopts, logger)
+
+main()
--- pymodule/core.py
+++ pymodule/core.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import argparse
-import daemon
 import datetime
 import psutil
 import pwd
@@ -10,17 +8,11 @@ import sys
 import threading
 import time
 
-from leftokill import log
 from leftokill import reportmail
-from leftokill import config
 
 homeprefix = '/home/'
-# conffile = 'leftokill.conf'
-conffile = '/etc/leftokill/leftokill.conf'
-confopt = dict()
 logger = None
 reported, report_leftovers = set(), dict()
-
 
 def term_and_kill(candidate):
     cgone, calive, pgone, palive = list(), list(), list(), list()
@@ -133,7 +125,7 @@ def build_report_syslog(leftovers, confopts):
 
     return msg
 
-def daemon_func(confopts):
+def run(confopts, logger):
     global report_leftovers
     lock = threading.Lock()
 
@@ -164,34 +156,3 @@ def daemon_func(confopts):
         lock.release()
 
         time.sleep(confopts['killeverysec'])
-
-def run():
-    global logger
-    lobj = log.Logger()
-    logger = lobj.get()
-
-    confopts = config.parse_config(conffile, logger)
-
-    for l in confopts['logmode']:
-        if l.lower() == 'syslog':
-            lobj.init_syslog()
-        if l.lower() == 'file':
-            lobj.init_filelog()
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', dest='nofork', action='store_true',
-                        help="do not fork into background",
-                        required=False)
-    args = parser.parse_args()
-
-    if args.nofork:
-        try:
-            daemon_func(confopts)
-        except KeyboardInterrupt:
-            raise SystemExit(1)
-    else:
-        context_daemon = daemon.DaemonContext()
-        with context_daemon:
-            daemon_func(confopts)
-
-run()
